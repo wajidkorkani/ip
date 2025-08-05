b5bb5429931eff2686a8c1fdcb805eb10ea2e810
Abdul Wajid Korkani <143551139+wajidkorkani@users.noreply.github.com>
2025-08-05 10:43:59 +0500

    HTML & JS

---
+++ index.js
@@ -0,0 +1,8 @@
+let ipLabel = document.querySelector(".ip")
+
+fetch("https://api.ipify.org?format=json")
+.then((data)=> data.json())
+.then((data) => ipLabel.innerHTML = data.ip)
+.catch((error) => ipLabel.innerHTML = error)
+
+
